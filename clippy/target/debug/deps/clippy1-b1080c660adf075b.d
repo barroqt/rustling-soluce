d468ed263cc5a377683c0863d5753a02ddf4c6bb
barroqt <barroqt@gmail.com>
2023-06-05 00:55:20 +0700

    macros

---
+++ clippy/target/debug/deps/clippy1-b1080c660adf075b.d
@@ -0,0 +1,8 @@
+/Users/thomas/Courses/Rust/rustlings/exercises/clippy/target/debug/deps/clippy1-b1080c660adf075b.rmeta: clippy1.rs Cargo.toml
+
+/Users/thomas/Courses/Rust/rustlings/exercises/clippy/target/debug/deps/clippy1-b1080c660adf075b.d: clippy1.rs Cargo.toml
+
+clippy1.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
